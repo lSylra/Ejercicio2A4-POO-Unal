4b5f0f6135fd5ad89d26f87aaaa274e2b1527036
lSylra <143262189+lSylra@users.noreply.github.com>
2023-10-16 19:54:27 -0500

    Add files via upload

---
+++ CasaIndependiente.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebainmuebles;
+
+/**
+ *
+ * @author USER
+ */
+public class CasaIndependiente extends CasaUrbana {
+    protected static double valorArea = 3000000;
+    public CasaIndependiente(int identificadorInmobiliario, int area, String direccion, int numeroHabitaciones, int numeroBaños, int numeroPisos) {
+    super(identificadorInmobiliario, area, direccion, numeroHabitaciones, numeroBaños, numeroPisos);
+    }
+    void imprimir() {
+        super.imprimir(); 
+        System.out.println();
+    }
+}
